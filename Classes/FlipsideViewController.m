4d507c18c6bdc9660da3736cf85d5ac2ce3d8638
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-09-04 12:40:14 +0200

    FlipSiteView accept portrait layout

--- Classes/FlipsideViewController.m
+++ Classes/FlipsideViewController.m
@@ -59,13 +59,12 @@
 }
 
 
-/*
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	// Return YES for supported orientations
-	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	// return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	return YES;
 }
-*/
 
 
 - (void)dealloc {
