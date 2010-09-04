a691a71f505b636ef6f3b946992319b1c74557f5
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-09-04 12:44:07 +0200

    WebsiteListView allowed to display in horizontal

--- Classes/WebsiteListViewController.m
+++ Classes/WebsiteListViewController.m
@@ -15,4 +15,11 @@
 	[self.view addSubview:[navigationController view]];
 }
 
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	// Return YES for supported orientations
+	// return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	return YES;
+}
+
 @end
